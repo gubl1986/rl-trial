83335bb5b65ef14b906e06679d53b063d2a4fbee
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2020-01-15 16:06:02 -0500

    Add stats and plot

--- rl-gym.py
+++ rl-gym.py
@@ -1,9 +1,16 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Make the car environment - It has 3 possible actions (l,r,nothing)
 env = gym.make("MountainCar-v0")
 
+# For saving stats
+ep_rewards = []
+aggr_ep_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
+STATS_EVERY = 100
+
+# Q-table settings
 DISCRETE_OS_SIZE = [20, 20]
 discrete_os_win_size = (env.observation_space.high-env.observation_space.low)/DISCRETE_OS_SIZE
 
@@ -19,7 +26,7 @@ END_EPSILON_DECAYING = EPISODES//2
 epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 # how often to render the environment (show every __ episodes)
-SHOW_EVERY = 3000
+SHOW_EVERY = 4000
 
 # Initialize q-table
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
@@ -31,6 +38,8 @@ def get_discrete_state(state):
     # note to self: tuples are immutable, so initially, the state is a list, and then it's converted to a tuple to be returned
 
 for episode in range(EPISODES):
+    episode_reward = 0
+
     # Prep the environment
     init_state = env.reset()
     discrete_state = get_discrete_state(init_state)
@@ -45,7 +54,6 @@ for episode in range(EPISODES):
     # do the things! run the simulation:
     done = False
     while not done:
-
         # choose the best q value action, or randomly explore:
         if np.random.random() > epsilon:
             # get action from q table
@@ -57,6 +65,8 @@ for episode in range(EPISODES):
 
         new_state, reward, done, _ = env.step(action)
         new_discrete_state = get_discrete_state(new_state)
+
+        episode_reward += reward
 
         # don't render every time:
         if render == True:
@@ -89,5 +99,21 @@ for episode in range(EPISODES):
     if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
         epsilon -= epsilon_decay_value
 
+    # Update stats
+    ep_rewards.append(episode_reward)
+    if not episode % STATS_EVERY:
+        average_reward = sum(ep_rewards[-STATS_EVERY:])/STATS_EVERY
+        aggr_ep_rewards['ep'].append(episode)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['max'].append(max(ep_rewards[-STATS_EVERY:]))
+        aggr_ep_rewards['min'].append(min(ep_rewards[-STATS_EVERY:]))
+        print(f'Episode: {episode:>5d}, average reward: {average_reward:>4.1f}, current epsilon: {epsilon:>1.2f}')
+
 env.close
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="average rewards")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max rewards")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min rewards")
+plt.legend(loc=4)
+plt.show()
 
